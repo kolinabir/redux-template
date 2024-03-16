5697da39c898a051fa24830d1c88b45b1c9ce3fa
kolinabir <knkolin9@gmail.com>
2024-03-17 01:44:53 +0600

    Update code formatting and fix minor bugs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>My TODO</h1>
+      <Button>YO</Button>
+    </div>
+  );
+}
+
+export default App;
